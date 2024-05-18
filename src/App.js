a7119050616b0a2a25435a6fcbf2823c14afa8b8
BabyCris21 <115188969+BabyCris21@users.noreply.github.com>
2024-05-17 21:06:57 -0500

    Cambio

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer"; // Importa el Footer
 import Home from "./Screens/Home";
+import Staff from './Screens/Staff';
 import Registro from "./Screens/Registro";
+import Login from './Screens/Login';
 import Contact from "./Screens/Contact"; // Importa el componente Contact desde la carpeta correcta
 
 function App() {
@@ -14,7 +16,9 @@ function App() {
         <Routes>
           <Route path="/home" element={<Home/>}/>
           <Route path="/contact" element={<Contact/>}/>
-          <Route path="/registro" element={<Registro/>} />
+          <Route path="/registro" element={<Registro/>}/>
+          <Route path="/staff" element={<Staff/>}/>
+          <Route path="/login" element={<Login/>}/>
         </Routes>
         <Footer /> 
       </Router>
