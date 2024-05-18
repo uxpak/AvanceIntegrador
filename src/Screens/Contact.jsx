179541a50b1f676754afa99e23507ab27a838eac
BabyCris21 <115188969+BabyCris21@users.noreply.github.com>
2024-05-17 21:18:03 -0500

    Actualizacion en los href

--- src/Screens/Contact.jsx
+++ src/Screens/Contact.jsx
@@ -1,38 +1,11 @@
-import React from "react";
-import { Link } from "react-router-dom"; // Importa Link desde react-router-dom
-import "./Contact.css";
+import React from 'react'
 
-const Login = () => {
-    return (
-        <div className="login-container">
-            <div className="company-photo"></div>
-            <div className="login-form">
-                <h1>Iniciar Sesión</h1>
-                <h4>¿No tienes una cuenta? <Link to="/registro">Crea una</Link></h4> 
-                <form>
-                    <div className="form-group">
-                        <label htmlFor="email">Email:</label>
-                        <input type="email" id="email" name="email" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password">Contraseña:</label>
-                        <input type="password" id="password" name="password" />
-                    </div>
-                    <button type="submit" className="login-button">INICIAR SESION</button>
-                </form>
-                <div className="forgot-password">
-                    <a href="#">¿Olvidaste tu contraseña?</a>
-                </div>
-                <div className="social-login">
-                    <p>O inicia sesión con:</p>
-                    <div className="social-buttons">
-                        <button className="google-button">Google</button>
-                        <button className="facebook-button">Facebook</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-};
+const Contact = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
 
-export default Login;
+export default Contact
--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -2,51 +2,11 @@ import React from 'react';
 import "./Home.css";
 
 const Home = () => {
-  // Supongamos que tienes información de médicos en un array
-  const medicalStaff = [
-    { id: 1, name: "Dr. Juan Pérez", specialization: "Cardiología", photo: "https://st3.depositphotos.com/1743476/32257/i/450/depositphotos_322579018-stock-photo-smiling-doctor-standing-on-grey.jpg" },
-    { id: 2, name: "Dra. María González", specialization: "Pediatría", photo: "https://media.istockphoto.com/id/1530248128/photo/portrait-of-cheerful-female-doctor-with-digital-tablet-beside-window-in-corridor.webp?b=1&s=170667a&w=0&k=20&c=lr9kQSJFSMXao-_XHEyTjca6fhzdoIg9lDdOL8j-kHE=" },
-    { id: 3, name: "Dr. José Rodríguez", specialization: "Dermatología", photo: "https://www.shutterstock.com/image-photo/healthcare-medical-staff-concept-portrait-600nw-2281024823.jpg" },
-    { id: 4, name: "Dra. Ana Martínez", specialization: "Ginecología", photo: "https://www.shutterstock.com/image-photo/profile-photo-attractive-family-doc-600nw-1724693776.jpg" },
-    { id: 5, name: "Dr. Carlos Sánchez", specialization: "Oftalmología", photo: "https://st2.depositphotos.com/4431055/11852/i/450/depositphotos_118522306-stock-photo-handsome-doctor-portrait.jpg" },
-    { id: 6, name: "Dra. Laura López", specialization: "Neurología", photo: "https://img.freepik.com/foto-gratis/doctora-vistiendo-bata-laboratorio-estetoscopio-aislado_1303-29791.jpg" },
-    { id: 7, name: "Dr. Carlos Sánchez", specialization: "Cirugia", photo: "https://st2.depositphotos.com/4431055/11852/i/450/depositphotos_118522306-stock-photo-handsome-doctor-portrait.jpg" },
-    { id: 8, name: "Dra. Laura López", specialization: "Geriatria", photo: "https://img.freepik.com/foto-gratis/doctora-vistiendo-bata-laboratorio-estetoscopio-aislado_1303-29791.jpg" },
-    { id: 9, name: "Dr. Carlos Sánchez", specialization: "Otorrino", photo: "https://st2.depositphotos.com/4431055/11852/i/450/depositphotos_118522306-stock-photo-handsome-doctor-portrait.jpg" },
-    
-  ];
 
-  return (
-    <div>
-      {/* Sección superior con imagen */}
-      <section className="top-section">
-        <div className="company-info">
-          <h1>VitalCare</h1>
-          <p>Cuidamos tu salud<br />Líderes en la excelencia médica</p>
-          <button>Nuestros servicios</button>
-        </div>
-        <div className="button-container">
-          <button>Reservar una cita</button>
-          <button>Ver staff médicos</button>
-          <button>Servicios</button>
-        </div>
-      </section>
+  return(
+    <div>Hola</div>
 
-      {/* Contenedor de staff médico */}
-      <div className="staff-container">
-        <h2>Nuestro Equipo Médico</h2>
-        <div className="staff-list">
-          {medicalStaff.map(doctor => (
-            <div key={doctor.id} className="doctor-card">
-              <img src={doctor.photo} alt={doctor.name} />
-              <h3>{doctor.name}</h3>
-              <p>Especialización: {doctor.specialization}</p>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+  )
 }
 
 export default Home;
