1afbdeb6e0718d0d2b6e77a879c2d65a8862f2c2
BabyCris21 <cristhianhernandez928@gmail.com>
2024-05-23 20:06:58 -0500

    Cambios

---
+++ src/Screens/serviceImages.js
@@ -0,0 +1,9 @@
+const serviceImages = {
+    "Chequeo Gratis": ["chequeo1.jpg", "chequeo2.webp"],
+    "Cardiograma": ["cardio1.jpg", "cardio2.webp"],
+    "Test de ADN": ["adn.jpeg", "adn2.jpg"],
+    "Banco de Sangre": ["sangre.jpg", "sangre2.jpg"]
+  };
+  
+  export default serviceImages;
+  
