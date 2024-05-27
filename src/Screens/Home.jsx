884ca9413702a4710fb9e4bd2604c2002d407117
BabyCris21 <115188969+BabyCris21@users.noreply.github.com>
2024-05-27 14:58:52 -0500

    Revert "Cambios"
    This reverts commit 1afbdeb6e0718d0d2b6e77a879c2d65a8862f2c2.

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -5,45 +5,77 @@ import "./Home.css";
 const Home = () => {
   const [selectedOption, setSelectedOption] = useState("Chequeo Gratis"); 
   const [showContent, setShowContent] = useState(true); 
-  const [specialties, setSpecialties] = useState([]); // Definición de especialidades
-  const [isPopupOpen, setIsPopupOpen] = useState(false); // Estado para controlar la apertura y el cierre del popup
 
   useEffect(() => {
     setShowContent(true); 
-    // Aquí deberías cargar las especialidades desde una fuente de datos
-    // Supongamos que fetchedSpecialties es una lista de especialidades obtenidas de alguna fuente
-    const fetchedSpecialties = [
-      { title: "Especialidad 1", image: "especialidad1.jpg" },
-      { title: "Especialidad 2", image: "especialidad2.jpg" },
-      { title: "Especialidad 2", image: "especialidad2.jpg" },
-      { title: "Especialidad 2", image: "especialidad2.jpg" },
-      { title: "Especialidad 2", image: "especialidad2.jpg" },
-      { title: "Especialidad 2", image: "especialidad2.jpg" },
-      { title: "Especialidad 2", image: "especialidad2.jpg" },
-      { title: "Especialidad 2", image: "especialidad2.jpg" },
-      { title: "Especialidad 2", image: "especialidad2.jpg" },
-      { title: "Especialidad 2", image: "especialidad2.jpg" },
-      { title: "Especialidad 2", image: "especialidad2.jpg" },
-      { title: "Especialidad 2", image: "especialidad2.jpg" },
-    ]; 
-    setSpecialties(fetchedSpecialties);
   }, []); 
+
 
   const handleOptionClick = (option) => {
     setSelectedOption(option);
     setShowContent(true);
   };
+
   
-  const handleSpecialtyClick = (specialty) => {
-    // Aquí puedes manejar el clic en una especialidad
-    // Por ejemplo, podrías abrir el popup con información sobre la especialidad
-    setIsPopupOpen(true);
+
+  const serviceInfo = {
+    "Chequeo Gratis": {
+      frase: "Obtenga tranquilidad sobre su salud con nuestro servicio de chequeo gratis. Nuestro equipo médico le ofrece una evaluación completa y sin costo para detectar posibles problemas de salud y promover su bienestar a largo plazo.",
+      description: "El chequeo gratis es crucial para detectar problemas de salud en etapas tempranas, lo que permite intervenir antes de que se conviertan en afecciones graves. Proporciona a los pacientes una evaluación completa de su estado de salud sin costo, fomentando una mayor conciencia y cuidado preventivo.",
+      items: [
+        "Detección temprana de enfermedades.",
+        "Prevención de complicaciones de salud.",
+        "Ahorro de costos a largo plazo.",
+        "Promoción de un estilo de vida saludable.",
+        "Tranquilidad y seguridad para los pacientes.",
+        "Acceso a orientación médica personalizada."
+      ]
+    },
+    "Cardiograma": {
+      frase: "Su corazón es el motor de su vida, y nuestro servicio de cardiograma está aquí para mantenerlo en óptimas condiciones. Con nuestra avanzada tecnología y experiencia médica, podemos evaluar su salud cardiovascular de manera precisa y ayudarlo a tomar medidas preventivas para un corazón más saludable.",
+      description: "El cardiograma es una herramienta fundamental para evaluar la salud del corazón. Proporciona información vital sobre la función cardíaca, ayudando a detectar y tratar problemas cardíacos en etapas tempranas, lo que puede salvar vidas y mejorar la calidad de vida de los pacientes.",
+      items: [
+        "Diagnóstico preciso de enfermedades genéticas.",
+        "Orientación en la toma de decisiones sobre salud y estilo de vida.",
+        "Identificación de riesgos hereditarios para la salud.",
+        "Personalización de tratamientos médicos.",
+        "Confirmación de la paternidad",
+        "Apoyo en la investigación médica y científica"
+      ]
+    },
+    "Test de ADN": {
+      frase: "Descubra los secretos de su genética con nuestra prueba de ADN. Nuestro servicio le brinda información invaluable sobre su predisposición genética a enfermedades, permitiéndole tomar decisiones informadas sobre su salud y estilo de vida.",
+      description: "Las pruebas de ADN ofrecen una ventana única hacia la genética de un individuo, lo que permite diagnosticar enfermedades genéticas, identificar riesgos hereditarios y personalizar tratamientos médicos. Además, tienen aplicaciones en campos como la paternidad y la investigación científica.",
+      items: [
+        "Diagnóstico preciso de enfermedades genéticas.",
+        "Identificación de riesgos hereditarios para la salud.",
+        "Personalización de tratamientos médicos.",
+        "Orientación en la toma de decisiones sobre salud y estilo de vida.",
+        "Confirmación de la paternidad.",
+        "Apoyo en la investigación médica y científica."
+      ]
+    },
+    "Banco de Sangre": {
+      frase: "Cuando se trata de emergencias médicas, tener acceso a un banco de sangre confiable puede marcar la diferencia entre la vida y la muerte. Nuestro banco de sangre proporciona un suministro vital para transfusiones, tratamientos y cirugías, garantizando que usted reciba la atención médica que necesita cuando más lo necesita.",
+      description: "Los bancos de sangre desempeñan un papel crucial en la atención médica al proporcionar sangre y componentes sanguíneos para transfusiones, tratamientos y cirugías. Son esenciales para salvar vidas en emergencias, así como para el tratamiento de enfermedades crónicas y el avance de la medicina a través de la investigación.",
+      items: [
+        "Suministro de sangre para transfusiones.",
+        "Soporte vital en emergencias y cirugías.",
+        "Tratamiento de enfermedades crónicas.",
+        "Ayuda a pacientes con trastornos de la coagulación.",
+        "Apoyo a pacientes en quimioterapia y trasplantes.",
+        "Contribución a la investigación médica y científica"
+      ]
+    }
   };
 
-  const handleClosePopup = () => {
-    setIsPopupOpen(false); // Cambia setIsPopOpen por setIsPopupOpen
-  }
-  
+  const serviceImages = {
+    "Chequeo Gratis": ["chequeo1.jpg", "chequeo2.webp"],
+    "Cardiograma": ["cardio1.jpg", "cardio2.webp"],
+    "Test de ADN": ["adn.jpeg", "adn2.jpg"],
+    "Banco de Sangre": ["sangre.jpg", "sangre2.jpg"]
+  };
+
   const contentClass = selectedOption && showContent ? 'show' : '';
 
   return (
@@ -80,11 +112,12 @@ const Home = () => {
           <button className={`home-service-button ${selectedOption === "Cardiograma" ? "selected-section" : ""}`} onClick={() => handleOptionClick("Cardiograma")}>Cardiograma</button>
           <button className={`home-service-button ${selectedOption === "Test de ADN" ? "selected-section" : ""}`} onClick={() => handleOptionClick("Test de ADN")}>Test de ADN</button>
           <button className={`home-service-button ${selectedOption === "Banco de Sangre" ? "selected-section" : ""}`} onClick={() => handleOptionClick("Banco de Sangre")}>Banco de Sangre</button>
+
           <button className="home-view-all-button">Ver todo</button>
         </div>
 
         {selectedOption && (
-          <div className={`home-service-info ${contentClass} home-service-info-container`}>
+          <div className={`home-service-info ${contentClass}`}>
             <h1>{selectedOption}</h1>
             <h3 className="service-info-text">{serviceInfo[selectedOption].frase}</h3>
             <ul className="service-info-list">
@@ -102,38 +135,9 @@ const Home = () => {
             <img src={serviceImages[selectedOption][1]} alt={`${selectedOption} - 2`} />
           </div>
         )}
-    
       </section>
-        
-      <section className="home-specialties-section">
-        <div className="home-specialties-title">
-          <h2>Siempre cuidando</h2>
-          <h1>Nuestras especialidades</h1>
-        </div>
-                <div className="home-specialties-container">
-          {/* Crear los botones con imágenes y descripciones */}
-          {/* Ejemplo: */}
-          {specialties.map((specialty, index) => (
-            <button key={index} onClick={() => handleSpecialtyClick(specialty)}>
-              <img src={specialty.image} alt={specialty.title} />
-            </button>
-          ))}
-        </div>
-      </section>
-
-      {/* Popup */}
-      {isPopupOpen && (
-        <div className="popup">
-          <div className="popup-content">
-            <h2>Popup Content</h2>
-            <p>This is some popup content.</p>
-            <button onClick={handleClosePopup}>Close Popup</button>
-          </div>
-        </div>
-      )}
     </div>
   );
 }
 
 export default Home;
-
--- src/Screens/serviceImages.js
+++
@@ -1,9 +0,0 @@
-const serviceImages = {
-    "Chequeo Gratis": ["chequeo1.jpg", "chequeo2.webp"],
-    "Cardiograma": ["cardio1.jpg", "cardio2.webp"],
-    "Test de ADN": ["adn.jpeg", "adn2.jpg"],
-    "Banco de Sangre": ["sangre.jpg", "sangre2.jpg"]
-  };
-  
-  export default serviceImages;
-  
